070cc1d8ddd8d4e30a5a2290f310b148f0fbfa41
Cheese <djmera@alumno.montepiedra.edu.ec>
2025-10-11 10:14:38 -0500

    Create Pedir dos números y mostrar cuál es mayor.cpp

---
+++ Pedir dos números y mostrar cuál es mayor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Ingrese dos numeros: ";
+    cin >> a >> b;
+
+    if (a > b)
+        cout << "El mayor es: " << a;
+    else if (b > a)
+        cout << "El mayor es: " << b;
+    else
+        cout << "Son iguales.";
+    return 0;
+}
