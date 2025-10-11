4de9b0dfba5628142bc6fc24b1e834a2faaa7367
Cheese <djmera@alumno.montepiedra.edu.ec>
2025-10-11 10:37:23 -0500

    Create Pedir un número y mostrar su valor absoluto.cpp

---
+++ Pedir un número y mostrar su valor absoluto.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    cout << "Valor absoluto: " << fabs(n);
+    return 0;
+}
